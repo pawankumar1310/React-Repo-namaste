f78114e3efe9121b2e20eed10167fc26f0dd2b67
pawankumar <pawankumar131098@gmail.com>
2025-06-08 17:40:57 +0530

    Create element using Html, javascript and React Episode-1

---
+++ App.js
@@ -0,0 +1,35 @@
+/*
+Create following element using React
+
+<div id="parent">
+    <div id="child1">
+        <h1>Namaste Javascript 1</h1>
+        <h2>Namaste Javscript 1</h1>
+    </div>
+    <div id="child2">
+        <h1>Namaste Javascript 2</h1>
+        <h2>Namaste Javscript 2</h1>
+    </div>
+</div>
+*/
+
+
+const parent = React.createElement(
+    'div', { id: 'parent' }, [
+        React.createElement(
+            'div', { id: 'child1' }, [
+                React.createElement('h1', {}, 'Namaste Javascript 1'),
+                React.createElement('h2', {}, 'Namaste Javascript 1'),
+            ]
+        ),
+        React.createElement(
+            'div', { id: 'child2' }, [
+                React.createElement('h1', {}, 'Namaste Javascript 2'),
+                React.createElement('h2', {}, 'Namaste Javascript 2'),
+            ]
+        )
+    ]
+)
+
+const rootReacts = ReactDOM.createRoot(document.getElementById('root1'));
+rootReacts.render(parent);
