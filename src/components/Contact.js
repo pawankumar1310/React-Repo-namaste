1803aa39c43bd8bbbf32d968fbd72ac64cf65b3c
Pawan <pawankumar131098@gmail.com>
2025-11-03 16:57:37 +0530

    styling in contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,15 @@
-const Contact = ()=>{
-    return(
+const Contact = () => {
+    return (
         <>
-            This is Contact Page...        
+            <div >
+                <h1 className="font-bold text-3xl m-2 p-2"> Contact Us</h1>
+                <form>
+                    <input type="text" placeholder="Name" className="border border-black m-2 p-2"></input>
+                    <input type="text" placeholder="Message" className="border border-black m-2 p-2"></input>
+                    <button className="border border-black rounded-lg m-2 p-2 cursor-pointer">Submit</button>
+                </form>
+            </div>
+
         </>
     )
 }
