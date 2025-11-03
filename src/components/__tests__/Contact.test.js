d474e9a4154d556fe72de3a1227334a887664747
Pawan <pawankumar131098@gmail.com>
2025-11-03 19:16:53 +0530

    testcase to load contact us component or not

---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react"
+import Contact from "../Contact"
+import "@testing-library/jest-dom"
+
+
+test("Should load Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+});
+
+
+test("Should load Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+});
+
+test("Should load input in Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const inputName = screen.getByPlaceholderText("Message");
+    expect(inputName).toBeInTheDocument();
+});
+
+test("Should load all input in Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(2);
+});
