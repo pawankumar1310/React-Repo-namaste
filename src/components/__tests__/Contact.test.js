47cc8648d480b28763ae93d74df48297ee853f84
Pawan <pawankumar131098@gmail.com>
2025-11-04 06:26:34 +0530

    test cases

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -31,3 +31,35 @@ test("Should load all input in Contact us component ", ()=>{
     const inputBoxes = screen.getAllByRole("textbox");
     expect(inputBoxes.length).toBe(2);
 });
+
+
+describe("Test cases in a Group", ()=>{
+    test("Should load Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+});
+
+
+test("Should load Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+});
+
+it("Should load input in Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const inputName = screen.getByPlaceholderText("Message");
+    expect(inputName).toBeInTheDocument();
+});
+
+it("Should load all input in Contact us component ", ()=>{
+    render(<Contact/>);
+
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).toBe(2);
+});
+})
